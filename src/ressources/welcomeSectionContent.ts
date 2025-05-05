bbacabf43ab7a114417af1db23306ace1bbe88a4
Maximilian Merten <19maximilianmerten96@gmail.com>
2025-05-05 14:55:11 +0200

    refactor/changes welcome text

--- src/ressources/welcomeSectionContent.ts
+++ src/ressources/welcomeSectionContent.ts
@@ -1,5 +1,5 @@
 export default {
     id: 1,
     name: "Maximilian Merten",
-    textContent: "Ich bin ein junger Fullstack-Entwickler mit Leidenschaft für smarte Lösungen und sauberen Code. Hier bekommst du einen Einblick in meine Projekte und Ideen – schön, dass du da bist!"
+    textContent: "I am a young full-stack developer with a passion for smart solutions and clean code. Here you can get a glimpse of my projects and ideas."
 }
